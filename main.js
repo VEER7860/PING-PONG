69f7b46e208fd29a122ebee8c37b9b28cd0b0696
VEER7860 <86776400+VEER7860@users.noreply.github.com>
2021-12-01 02:28:36 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,41 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+  
+    createCanvas(650, 400);
+    video = createCapture(VIDEO);
+    video.size(600,300);
+    
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+  }
+  
+  function draw() {
+  background("#D3D3D3");
+     if(noseX < 300)
+     {
+        marioX = marioX - 1;
+     }
+     if(noseX > 300)
+     {
+       marioX = marioX + 1 ;
+     }
+     if(noseY < 150)
+     {
+       marioY = marioY - 1;
+     }
+  
+     image(img,marioX, marioY, 40,70);
+  }
+  function gotPoses(results)
+  {
+    if(results.lenght > 0)
+  {
+    noseX = results[0].pose.nose.x;
+    noseY = resuults[0].pose.nose.y;
+    console.log("noseX = " + noseX +", noseY =" + noseY);
+  
+    }
 }
 
 
